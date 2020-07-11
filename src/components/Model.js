5548e03349f4cc3d117e5cecaecbf59289ec5db4
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 16:13:26 -0400

    updated headers and added BookingsContainer

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import BookingsContainer from '../containers/BookingsContainer';
 
 const Model = (props) => {
   let model = props.models[props.match.params.id -1];
 
   return (
-    <h2>{model ? model.name : null}
-      {model ? model.picture : null} -
-      {model ? model.height : null} -
-      {model ? model.bust : null} -
-      {model ? model.waist : null} -
-      {model ? model.hip : null} -
-      {model ? model.shoe : null} -
-      {model ? model.eyes : null} -
-      {model ? model.hair : null}
-    </h2>
+    <div>
+      <h2>{model ? model.name : null}</h2>
+      <h3>
+        {model ? model.picture : null} -
+        {model ? model.height : null} -
+        {model ? model.bust : null} -
+        {model ? model.waist : null} -
+        {model ? model.hip : null} -
+        {model ? model.shoe : null} -
+        {model ? model.eyes : null} -
+        {model ? model.hair : null}
+      </h3>
+      <BookingsContainer />
+    </div>
   );
 }
 
