8c5476d7cc490a50bfb1e5bde1e2f6efef3cb3c1
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 15:19:36 -0400

    updated props

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 const Model = (props) => {
+  let model = props.models[props.match.params.id -1];
+
   return (
     <li>
-      {props.model.name} -
-      {props.model.picture} -
-      {props.model.height} -
-      {props.model.bust} -
-      {props.model.waist} -
-      {props.model.hip} -
-      {props.model.shoe} -
-      {props.model.eyes} -
-      {props.model.hair}
+      {model ? model.name : null} -
+      {model ? model.picture : null} -
+      {model ? model.height : null} -
+      {model ? model.bust : null} -
+      {model ? model.waist : null} -
+      {model ? model.hip : null} -
+      {model ? model.shoe : null} -
+      {model ? model.eyes : null} -
+      {model ? model.hair : null}
     </li>
   );
 }
