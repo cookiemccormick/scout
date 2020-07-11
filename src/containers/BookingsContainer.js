f5bd22938fe253951cdbd6f8f0bceecb4e438c1e
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 16:31:50 -0400

    created Bookings component

--- src/containers/BookingsContainer.js
+++ src/containers/BookingsContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import BookingInput from '../components/BookingInput';
+import Bookings from '../components/Bookings';
+import Model from '../components/Model';
 
 class BookingsContainer extends React.Component {
   render() {
     return (
       <div>
         <BookingInput />
+        <Bookings bookings={this.props.models && this.props.model.bookings} />
       </div>
     );
   }
