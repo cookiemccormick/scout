839213960873640367d0839d8e67f1a7201f1094
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 09:30:28 -0400

    removed notes

--- src/components/Models.js
+++ src/components/Models.js
@@ -4,30 +4,19 @@ const Models = (props) => {
   return (
     <div>
       {props.models.map(model =>
-      <li key={model.id}>
-        {model.name} -
-        {model.pictures} -
-        {model.height} -
-        {model.bust} -
-        {model.waist} -
-        {model.hip} -
-        {model.shoe} -
-        {model.eyes} -
-        {model.hair}
-      </li>)}
+        <li key={model.id}>
+          {model.name} -
+          {model.pictures} -
+          {model.height} -
+          {model.bust} -
+          {model.waist} -
+          {model.hip} -
+          {model.shoe} -
+          {model.eyes} -
+          {model.hair}
+        </li>)}
     </div>
   );
 }
 
 export default Models;
-
-// model = Model.create!(
-//   name: 'Ashley Novak',
-//   pictures: '/images/ashleynovak.jpg',
-//   height: 68,
-//   bust: 34,
-//   waist: 24,
-//   hip: 35,
-//   shoe: 8,
-//   eyes: 'hazel',
-//   hair: 'blond'
