2a06cf12af37625f66822d2e028289fa9b5329e9
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 12:41:54 -0400

    updated form

--- src/components/ModelInput.js
+++ src/components/ModelInput.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {addModel} from '../actions/addModel';
 
 class ModelInput extends React.Component {
   state = {
@@ -15,36 +17,42 @@ class ModelInput extends React.Component {
 
   handleOnChange = (event) => {
     this.setState({
-      name: event.target.value
-    })
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addModel(this.state);
   }
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleOnSubmit}>
           <label>Model Name: </label>
           <input type='text'placeholder='name' value={this.state.name} onChange={this.handleOnChange} name='name'/><br/>
           <label>Select Image: </label>
-          <input type='file' placeholder='picture' value={this.state.picture} onChange={this.handleChange} name='picture'/><br/>
+          <input type='file' placeholder='picture' value={this.state.picture} onChange={this.handleOnChange} name='picture'/><br/>
           <label>Height: </label>
-          <input type='number' placeholder='height in inches' value={this.state.height} onChange={this.handleChange} name='height'/><br/>
+          <input type='number' placeholder='height in inches' value={this.state.height} onChange={this.handleOnChange} name='height'/><br/>
           <label>Bust: </label>
-          <input type='number' placeholder='bust' value={this.state.bust} onChange={this.handleChange} name='bust'/><br/>
+          <input type='number' placeholder='bust' value={this.state.bust} onChange={this.handleOnChange} name='bust'/><br/>
           <label>Waist: </label>
-          <input type='number' placeholder='waist' value={this.state.waist} onChange={this.handleChange} name='waist'/><br/>
+          <input type='number' placeholder='waist' value={this.state.waist} onChange={this.handleOnChange} name='waist'/><br/>
           <label>Hip: </label>
-          <input type='number' placeholder='hip' value={this.state.hip} onChange={this.handleChange} name='hip'/><br/>
+          <input type='number' placeholder='hip' value={this.state.hip} onChange={this.handleOnChange} name='hip'/><br/>
           <label>Shoe: </label>
-          <input type='number' placeholder='shoe' value={this.state.shoe} onChange={this.handleChange} name='shoe'/><br/>
+          <input type='number' placeholder='shoe' value={this.state.shoe} onChange={this.handleOnChange} name='shoe'/><br/>
           <label>Eyes: </label>
-          <input type='text' placeholder='eyes' value={this.state.eyes} onChange={this.handleChange} name='eyes'/><br/>
+          <input type='text' placeholder='eyes' value={this.state.eyes} onChange={this.handleOnChange} name='eyes'/><br/>
           <label>Hair: </label>
-          <input type='text' placeholder='hair' value={this.state.hair} onChange={this.handleChange} name='hair'/><br/>
+          <input type='text' placeholder='hair' value={this.state.hair} onChange={this.handleOnChange} name='hair'/><br/>
+          <input type='submit'/>
         </form>
       </div>
     );
   }
 }
 
-export default ModelInput;
+export default connect(null, {addModel})(ModelInput);
