56db80251f559642b155b7fde4976333f03119aa
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-11 16:17:51 -0400

    created BookingInput component

---
+++ src/components/BookingInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BookingInput extends React.Component {
+  render() {
+    return (
+      <div>
+        BookingInput
+      </div>
+    );
+  }
+}
+
+export default BookingInput;
--- src/containers/BookingsContainer.js
+++ src/containers/BookingsContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import BookingInput from '../components/BookingInput';
 
 class BookingsContainer extends React.Component {
   render() {
     return (
       <div>
-        BookingsContainer
+        <BookingInput />
       </div>
     );
   }
