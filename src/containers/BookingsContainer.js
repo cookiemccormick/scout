5b71e9427050b555e2e31e6eeb182a299d778b4f
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-12 12:38:26 -0400

    removed import Model and udpdated BookingInput

--- src/containers/BookingsContainer.js
+++ src/containers/BookingsContainer.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import BookingInput from '../components/BookingInput';
 import Bookings from '../components/Bookings';
-import Model from '../components/Model';
 
 class BookingsContainer extends React.Component {
   render() {
     return (
       <div>
-        <BookingInput />
+        <BookingInput model={this.props.model} />
         <Bookings bookings={this.props.model && this.props.model.bookings} />
       </div>
     );
