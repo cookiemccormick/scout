a2c1595f4eb5d87455781dcb8a15412cba5a4733
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-12 10:58:56 -0400

    created BookingInput form

--- src/components/BookingInput.js
+++ src/components/BookingInput.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class BookingInput extends React.Component {
+  state = {
+    job: '',
+    amount: '',
+    start_time: '',
+    end_time: '',
+    description: ''
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        BookingInput
+        <form>
+          <label>Job: </label>
+          <input type='text' placeholder='job' value={this.state.job} onChange={this.handleOnChange} name='job'/><br/>
+          <label>Amount: </label>
+          <input type='number' placeholder='amount' value={this.state.amount} onChange={this.handleOnChange} name='amount'/><br/>
+          <label>Start Time: </label>
+          <input type='datetime-local' placeholder='start time' value={this.state.start_time} onChange={this.handleOnChange} name='start_time'/><br/>
+          <label>End Time: </label>
+          <input type='datetime-local' placeholder='end time' value={this.state.end_time} onChange={this.handleOnChange} name='end_time'/><br/>
+          <label>Description: </label>
+          <input type='text' placeholder='description' value={this.state.description} onChange={this.handleOnChange} name='description'/><br/>
+        </form>
       </div>
     );
   }
 }
 
-export default BookingInput;
+export default connect(null)(BookingInput);
