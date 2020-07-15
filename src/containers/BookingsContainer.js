06311312fea16bc5b641e19a1518047288095978
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-07-14 20:54:16 -0400

    updated html

--- src/containers/BookingsContainer.js
+++ src/containers/BookingsContainer.js
@@ -8,26 +8,12 @@ class BookingsContainer extends React.Component {
   render() {
     return (
       <div>
-        <BookingInput model={this.props.model} />
         <Bookings bookings={this.props.model && this.props.model.bookings} />
+        <h3>Add Booking</h3>
+        <BookingInput model={this.props.model} />
       </div>
     );
   }
 }
 
-
 export default connect(null)(BookingsContainer);
-
-
-
-// render() {
-//   return (
-//     <div>
-//       <Switch>
-//         <Route path='/models/new' component={ModelInput} />
-//         <Route path='/models/:id' render={(routerProps) => <Model {...routerProps} models={this.props.models} />} />
-//         <Route path='/models' render={(routerProps) => <Models {...routerProps} models={this.props.models} />} />
-//     </Switch>
-//     </div>
-//   );
-// }
